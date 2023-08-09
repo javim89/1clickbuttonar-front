0d124384147dc29db5054c99d3453303b9f2f0a1
Javi <javier.marengo@devbase.us>
2023-08-09 15:59:53 -0300

    feat: load users

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useQuery, gql } from "@apollo/client";
 
+import "./App.css";
+
+const GET_USERS = gql`
+  query {
+    getAllUsers {
+      id
+      name
+      surname
+    }
+  }
+`;
+
+const ShowUsers = () => {
+  const { loading, error, data } = useQuery(GET_USERS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error : {error.message}</p>;
+
+  return data.getAllUsers.map((user: UserI) => <span>{user.surname}</span>);
+};
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ShowUsers />
     </div>
   );
 }
---
+++ src/types/global.d.ts
@@ -0,0 +1,9 @@
+export {} 
+
+declare global {
+  interface UserI {
+    id: String;
+    name: String;
+    surname: String;
+  }
+}
